71af54272400f6778fb05eb55b0784b48ccd1509
Spencer Wallace <dude.wallace@gmail.com>
2018-06-13 12:37:51 -0600

    Loading component

---
+++ src/components/LoadingRecipes.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class LoadingRecipes extends Component {
+    render() {
+        return (
+            <div>Hello</div>
+        )
+    }
+}
+
+export default LoadingRecipes
